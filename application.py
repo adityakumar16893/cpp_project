491c9a4d15cf4258cb3558ad22743415f7fa9520
Aditya Kumar <95876682+adityakumar16893@users.noreply.github.com>
2021-12-16 16:00:44 +0000

    Update application.py

--- application.py
+++ application.py
@@ -33,5 +33,5 @@ application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:python123
 db = SQLAlchemy(application)
 
 if __name__ == '__main__':
-    application = CppMain(application)
+    app = CppMain(application)
     application.run(host='0.0.0.0', port=7000, debug=True)
